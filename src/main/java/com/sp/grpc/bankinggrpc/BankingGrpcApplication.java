a9fdb9db930f90a522c6c04ac2948cf7a6a5dff5
sharadpawar01 <sharadpawar01@gmail.com>
2023-02-27 21:50:10 +0530

    Initial commit

---
+++ src/main/java/com/sp/grpc/bankinggrpc/BankingGrpcApplication.java
@@ -0,0 +1,13 @@
+package com.sp.grpc.bankinggrpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingGrpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingGrpcApplication.class, args);
+	}
+
+}
